a154b617c17ecbb8c0d6ca4f8bb5016210c482d4
Sean Murphy <sean@iamseanmurphy.com>
2025-04-22 09:18:33 +0000

    2.9.0

--- easy-login-woocommerce/xoo-el-main.php
+++ easy-login-woocommerce/xoo-el-main.php
@@ -3,7 +3,7 @@
 * Plugin Name: Login/Signup Popup
 * Plugin URI: http://xootix.com/easy-login-for-woocommerce
 * Author: XootiX
-* Version: 2.8.9
+* Version: 2.9.0
 * Text Domain: easy-login-woocommerce
 * Domain Path: /languages
 * Author URI: http://xootix.com
